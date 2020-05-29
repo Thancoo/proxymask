9a25686ea5ee8ea9d5c0def9b92938c7ddbad14b
vadonical <vadonical@gmail.com>
2020-05-29 22:57:06 +0800

    Just for test

--- oracle/find.py
+++ oracle/find.py
@@ -3,3 +3,17 @@
 # @Time     : 2020/5/18 18:34
 # @File     : find.py
 # @IDE      : PyCharm
+
+
+class OracleFindData:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def find_schema(self):
+        pass
+
+    def find_database(self):
+        pass
+
+    def find_table(self):
+        pass
