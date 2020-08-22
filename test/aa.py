37f52911425159c434a7091b286bb86ef06b94d5
vadonical <vadonical@gmail.com>
2020-08-22 11:16:08 +0800

    test

--- test/aa.py
+++ test/aa.py
@@ -1,30 +1,6 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time     : 2020/4/15 17:44
+# @Time     : 2020/8/1 7:47 下午
+# @Author   : vadon
+# @Email    : vadonical@gmail.com
 # @File     : aa.py
-# @IDE      : PyCharm
-
-
-import string
-import math
-
-
-def is_odd(n):
-    return n % 2 == 1
-
-
-def is_sqr(x):
-    return math.sqrt(x) % 1 == 0
-
-
-def check_soldiers(n):
-    return n % 3 == 2 and n % 5 == 3 and n % 7 == 2
-
-
-temp = filter(is_odd, [1, 2, 3, 4, 5, 6, 7, 8, 9])
-tt = filter(is_sqr, range(1, 101))
-ss = filter(check_soldiers, range(1000, 1500))
-print(list(ss))
-
-print(temp)
-print(list(temp))
+# @Project  : proxymask
